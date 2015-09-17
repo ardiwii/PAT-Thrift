68f65c1e396a51b6f27fd832ac2588ae2b2cf59b
ardiwii <ardi.wii@gmail.com>
2015-09-17 16:38:55 +0700

    First Commit
    panduan yang di kuliah.informatika.org udah ditaro semua udah jalan

---
+++ src/if4031/CalculatorClient.java
@@ -0,0 +1,27 @@
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import if4031.CalculatorService;
+public class CalculatorClient {
+	public static void main(String [] args) {
+		try {
+			TTransport transport;
+			transport = new TSocket("localhost", 9090);
+			transport.open();
+			TProtocol protocol = new TBinaryProtocol(transport);
+			CalculatorService.Client client = new CalculatorService.Client(protocol);
+			perform(client);
+			transport.close();
+		} catch (TException x) {
+			x.printStackTrace();
+		}
+	}
+
+private static void perform(CalculatorService.Client client) throws TException
+	{
+		int product = client.multiply(3,5);
+		System.out.println("3*5=" + product);
+	}
+}
