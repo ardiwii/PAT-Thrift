7116326836cb639d1b81d76e33524016f4aaa443
ardiwii <ardi.wii@gmail.com>
2015-09-19 22:38:05 +0700

    coba lagi deh waha

--- src/if4031/ChatClient.java
+++ src/if4031/ChatClient.java
@@ -1,5 +1,6 @@
 package if4031;
 
+import java.util.Scanner;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
@@ -23,12 +24,39 @@ public class ChatClient {
 
 private static void perform(ChatService.Client client) throws TException
 	{
-            String nick = "noob";
-            String channel = "chan";
-            String channel2 = "";
-            client.joinChannel(nick, channel);
-            client.send(nick, "ngapain?",channel2);
-            System.out.println(client.recv(nick));
+            String nickname = "<unnamed>";
+            String input = "";
+            String[] parsed_msg;
+            Scanner scanner = new Scanner(System.in);
             
+            while(!input.equalsIgnoreCase("/exit")){
+                System.out.print(nickname + ">>");
+                input = scanner.nextLine();
+                parsed_msg = input.split(" ");
+                   
+                if(input.startsWith("/NICK")){
+                    nickname = parsed_msg[1];
+                }
+                else if(input.startsWith("/JOIN")){
+                    System.out.println("joining channel: " + parsed_msg[1]);
+                    client.joinChannel(nickname, parsed_msg[1]);
+                }
+                else if(input.startsWith("/LEAVE")){
+                    //TODO : implement leave channel
+                }
+                else if(input.startsWith("@")){
+                    input = input.substring(input.indexOf(" "));
+                    System.out.println("to channel: " + parsed_msg[0].substring(1) + " message: " + input);
+                    client.send(nickname, input, parsed_msg[0].substring(1));
+                }
+                else if(input.equalsIgnoreCase("/exit")){
+                    break;
+                }
+                else{
+                    System.out.println("to all channels: " + " message: " + input);
+                    client.send(nickname,input,"");
+                }
+                System.out.println(client.recv(nickname));
+            }            
 	}
 }
